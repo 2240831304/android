6db6f5bde8a52992c3b9b4acb30d60d67cf47bbc
liangman <2240831304@qq.com>
2020-07-05 19:06:19 +0800

    修改：     stock/app/build.gradle 	修改：     stock/app/src/main/java/com/example/stock/MainActivity.java 	修改：     stock/app/src/main/res/menu/bottom_nav_menu.xml 	修改：     stock/app/src/main/res/navigation/mobile_navigation.xml 	修改：     stock/app/src/main/res/values/strings.xml

--- stock/app/src/main/java/com/example/stock/MainActivity.java
+++ stock/app/src/main/java/com/example/stock/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 //                R.id.bottom_menu_news).build();
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.bottom_menu_news,R.id.bottom_menu_stockpick).build();
+                R.id.bottom_menu_news,R.id.bottom_menu_quotation,
+                R.id.bottom_menu_stockpick).build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
