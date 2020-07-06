3c98c3616553f7e205d0ad42e6e4eadc0521bd3a
liangman <liangman@obook.com.cn>
2020-07-06 15:34:44 +0800

    修改：     app/build.gradle 	修改：     app/src/main/java/com/example/stock/MainActivity.java 	新文件：   app/src/main/java/com/example/stock/ui/menunews/menunewsfragment.java 	修改：     app/src/main/res/navigation/mobile_navigation.xml

--- stock/app/src/main/java/com/example/stock/MainActivity.java
+++ stock/app/src/main/java/com/example/stock/MainActivity.java
@@ -19,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
-//                R.id.bottom_menu_news).build();
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.bottom_menu_news,R.id.bottom_menu_quotation,
