98f1394818821d7abe16846fa4f3e46e00ec0d7c
liangman <liangman@obook.com.cn>
2020-07-08 12:07:07 +0800

    修改：     app/src/main/java/com/example/stock/ui/stockpick/StockPickFrament.java 	修改：     app/src/main/res/layout/stockpick.xml

--- stock/app/src/main/java/com/example/stock/ui/stockpick/StockPickFrament.java
+++ stock/app/src/main/java/com/example/stock/ui/stockpick/StockPickFrament.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -26,23 +27,36 @@ public class StockPickFrament extends Fragment {
     private String[] NameList = new String[]{"沪市","中小板","创业板","科创板",
             "沪市","中小板","创业板","科创板"};
 
+    public StockPickFrament(){
+
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         this.content = getActivity().getApplicationContext();
 
         View root = inflater.inflate(R.layout.stockpick, container, false);
-        final GridView gridView = root.findViewById(R.id.stockpick_gridview);
-
         stockPickAdapter = new StockPickAdapter(this.content,this.NameList);
-        gridView.setAdapter(stockPickAdapter);
 
+        //final GridView gridView = root.findViewById(R.id.stockpick_gridview);
+        //gridView.setAdapter(stockPickAdapter);
+
+        /*
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(getActivity(), "position:"+i, Toast.LENGTH_SHORT).show();
             }
         });
+        */
+
+        GridLayout addGridLayout = root.findViewById(R.id.stockpick_addgridlayout);
+        for (int i = 0;i < NameList.length;++i) {
+            Button nameBut = new Button(this.content);
+            nameBut.setText(NameList[i]);
+            addGridLayout.addView(nameBut);
+        }
 
         return root;
     }
