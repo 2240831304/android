5bada4fb7937c1e4e80f7206140f4c4470d13a6c
liangman <2240831304@qq.com>
2020-07-14 23:29:38 +0800

    修改：     stock/app/src/main/AndroidManifest.xml 	修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java

--- stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java
@@ -3,6 +3,7 @@ package com.example.stock.eink.pioneer.stockadd;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.example.stock.MyApplication;
 import com.example.stock.eink.lib.filesystem.ConfigParameter;
 import com.example.stock.eink.lib.filesystem.FileSystemManager;
 import com.example.stock.eink.lib.sqlite.MySqliteOpenHelper;
@@ -17,17 +18,26 @@ public class StockAddDataSave {
     private String tablename;
     private String databaseFilePath;
     private static final String Class_Name = "org.sqlite.JDBC";
-
+    private MyApplication normal;
 
     public StockAddDataSave(String tableName) {
         tablename = tableName;
         FileSystemManager fileManeger = new FileSystemManager();
         databaseFilePath = fileManeger.getStockFilePath() + databaseName;
+        normal = new MyApplication();
     }
 
     public void save(){
         try {
 
+            int stockVersion = ConfigParameter.StockDatabaseVersion;
+            MySqliteOpenHelper oh = new MySqliteOpenHelper(normal.getContext(), databaseFilePath,
+                    null, stockVersion);
+            SQLiteDatabase db = oh.getWritableDatabase();
+            String sql = "INSERT INTO shanghai(name,code) VALUES(\"swsws\",\"frfrfrf\")";
+
+            db.execSQL(sql);
+            db.close();
 
         }catch (Exception e){
             System.out.println("lianjieshuju:" + e);
