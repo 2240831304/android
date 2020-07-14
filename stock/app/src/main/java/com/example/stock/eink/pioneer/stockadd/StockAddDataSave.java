579a07843b5c9b7335b1f29353900bbfe47f3338
liangman <liangman@obook.com.cn>
2020-07-14 18:45:52 +0800

    修改：     stock/app/build.gradle 	修改：     stock/app/src/main/AndroidManifest.xml 	新文件：   stock/app/src/main/java/com/example/stock/MyApplication.java 	修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java

--- stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/StockAddDataSave.java
@@ -7,21 +7,33 @@ import com.example.stock.eink.lib.filesystem.ConfigParameter;
 import com.example.stock.eink.lib.filesystem.FileSystemManager;
 import com.example.stock.eink.lib.sqlite.MySqliteOpenHelper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class StockAddDataSave {
 
     private String databaseName = "stock.db";
     private String tablename;
     private String databaseFilePath;
+    private static final String Class_Name = "org.sqlite.JDBC";
 
 
     public StockAddDataSave(String tableName) {
         tablename = tableName;
         FileSystemManager fileManeger = new FileSystemManager();
         databaseFilePath = fileManeger.getStockFilePath() + databaseName;
-
     }
 
     public void save(){
+        try {
+
+
+        }catch (Exception e){
+            System.out.println("lianjieshuju:" + e);
+            e.printStackTrace();
+        }
+
 
     }
 
