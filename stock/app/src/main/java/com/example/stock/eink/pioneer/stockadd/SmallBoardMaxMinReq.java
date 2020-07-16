363527555893a0203efb020ba4b9d333f4cf030c
liangman <liangman@obook.com.cn>
2020-07-16 16:14:05 +0800

    新文件：   stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/SmallBoardMaxMinReq.java 	修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/YearMaxMinPriceReq.java

---
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/SmallBoardMaxMinReq.java
@@ -0,0 +1,173 @@
+package com.example.stock.eink.pioneer.stockadd;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.stock.MyApplication;
+import com.example.stock.eink.lib.filesystem.ConfigParameter;
+import com.example.stock.eink.lib.filesystem.FileSystemManager;
+import com.example.stock.eink.lib.networkview.HttpRequest;
+import com.example.stock.eink.lib.sqlite.MySqliteOpenHelper;
+
+import java.net.HttpURLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SmallBoardMaxMinReq extends HttpRequest {
+
+    private static final String requesturl =
+            "https://q.stock.sohu.com/hisHq?code=cn_%1&start=%2&end=%3&stat=0&order=D&period=m";
+
+    private int startReqCode = 0;
+    private int executeIndexId = 0;
+    private int tableMaxIndex = 0;
+    private String executeReqCode = null;
+    private String StartDate = null;
+    private String EndData = null;
+
+    private String resultData = "";
+    private boolean requestState = false;
+
+    private String databaseName = "stock.db";
+    private String databaseFilePath;
+    private MyApplication normalActivity;
+    private SQLiteDatabase db;
+
+    private YearMaxMinPriceDataParse parser;
+    private YearMaxMinPriceDataSave  saver;
+
+    public SmallBoardMaxMinReq(int startCode,String startDatePt){
+        startReqCode = startCode;
+        StartDate = startDatePt;
+        FileSystemManager fileManeger = new FileSystemManager();
+        databaseFilePath = fileManeger.getStockFilePath() + databaseName;
+        normalActivity = new MyApplication();
+
+        int stockVersion = ConfigParameter.StockDatabaseVersion;
+        MySqliteOpenHelper oh = new MySqliteOpenHelper(normalActivity.getContext(), databaseFilePath,
+                null, stockVersion);
+        db = oh.getWritableDatabase();
+
+        parser = new YearMaxMinPriceDataParse();
+        saver = new YearMaxMinPriceDataSave(db,"smallboard");
+    }
+
+    public void fillHead(HttpURLConnection connecter)
+    {
+
+    }
+
+    public void addData(String data){
+        resultData += data;
+    }
+
+    public void parseData(){
+
+        if(getErrorMeg().isEmpty()){
+            if(resultData.contains("hq")){
+                parser.parse(resultData);
+                saver.save(executeIndexId,parser.getMinprice(),parser.getMaxprice());
+            }
+        }
+
+        resultData = "";
+
+    }
+
+    public void startReq() {
+        InitReqStockCode();
+        requestState = true;
+
+        while (requestState){
+            if(executeIndexId > tableMaxIndex){
+                break;
+            }
+
+            obtainReqStockCode();
+            if (executeReqCode == null){
+                executeIndexId += 1;
+                continue;
+            }
+
+            executeReqCode = executeReqCode.replace("sz","");
+
+            String url = requesturl.replace("%2",StartDate);
+            url = url.replace("%3",EndData);
+            url = url.replace("%1",executeReqCode);
+
+            System.out.println("SmallBoardMaxMinReq executeIndexId="+Integer.toString(executeIndexId));
+
+            setReqUrl(url);
+            execute();
+
+            executeIndexId += 1;
+        }
+
+        db.close();
+    }
+
+
+    public void stopReq()
+    {
+        requestState = false;
+    }
+
+    private void InitReqStockCode(){
+        String codeTemp = "sz" + getFullStockCode(startReqCode);
+        Cursor cursor = db.query("smallboard", new String[] {"id"},"code=?",
+                new String[] { codeTemp }, null, null, null);
+        // 将光标移动到下一行，从而判断该结果集是否还有下一条数据，如果有则返回true，没有则返回false
+        while (cursor.moveToNext()) {
+            executeIndexId = cursor.getInt(cursor.getColumnIndex("id"));
+        }
+
+        if (executeIndexId == 0){
+            executeIndexId = 1;
+        }else {
+            executeReqCode = codeTemp;
+        }
+
+        cursor = db.query("smallboard", new String[] {"MAX(id)"},null,
+                null, null, null, null);
+        // 将光标移动到下一行，从而判断该结果集是否还有下一条数据，如果有则返回true，没有则返回false
+        while (cursor.moveToNext()) {
+            tableMaxIndex = cursor.getInt(cursor.getColumnIndex("MAX(id)"));
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate = dateFormat.format(new Date());
+        EndData = currentDate.replace("-","");
+
+        System.out.println("SmallBoardMaxMinReq start executeIndexId="+Integer.toString(executeIndexId));
+        System.out.println("SmallBoardMaxMinReq start executeReqCode="+executeReqCode);
+        System.out.println("SmallBoardMaxMinReq table tableMaxIndex="+Integer.toString(tableMaxIndex));
+    }
+
+    private void obtainReqStockCode(){
+        executeReqCode = "";
+
+        Cursor cursor = db.query("smallboard", new String[] {"code"},"id=?",
+                new String[] { Integer.toString(executeIndexId) }, null, null, null);
+        // 将光标移动到下一行，从而判断该结果集是否还有下一条数据，如果有则返回true，没有则返回false
+        while (cursor.moveToNext()) {
+            executeReqCode = cursor.getString(cursor.getColumnIndex("code"));
+        }
+    }
+
+
+    private String getFullStockCode(int data){
+        String tmpStr = Integer.toString(data);
+        if(tmpStr.length() == 1){
+            tmpStr = "00000" + tmpStr;
+        }else if (tmpStr.length() == 2){
+            tmpStr = "0000" + tmpStr;
+        }else if (tmpStr.length() == 3){
+            tmpStr = "000" + tmpStr;
+        }else if (tmpStr.length() == 4){
+            tmpStr = "00" + tmpStr;
+        }
+        return tmpStr;
+    }
+
+}
+
--- stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/YearMaxMinPriceReq.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stockadd/YearMaxMinPriceReq.java
@@ -25,6 +25,9 @@ public class YearMaxMinPriceReq {
         if( stockBoardCnName.equals("沪市A股") ){
             startStockCodeID = context.getResources().getInteger(R.integer.shanghai_yearmaxmin_price_id);
             requestPt = new ShanghaiYearMaxMinReq(startStockCodeID,startDate);
+        }else if(stockBoardCnName.equals("中小板")){
+            startStockCodeID = context.getResources().getInteger(R.integer.shenzhen_yearmaxmin_price_id);
+            requestPt = new SmallBoardMaxMinReq(startStockCodeID,startDate);
         }
     }
 
