0a33f14311e82f0f58471c2d305e6fa4e4668a50
liangman <liangman@obook.com.cn>
2020-07-18 17:28:36 +0800

    新文件：   stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockLookupActivity.java 	新文件：   stock/app/src/main/res/layout/stock_lookup_layout.xml

---
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockLookupActivity.java
@@ -0,0 +1,15 @@
+package com.example.stock.eink.pioneer.stocklookup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.stock.R;
+
+public class StockLookupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stock_lookup_layout);
+    }
+}
