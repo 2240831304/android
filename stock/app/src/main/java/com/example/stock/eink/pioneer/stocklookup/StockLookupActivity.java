983a1cc9ea1e30f4c39ce1ee7a6fa16d1c73cb71
liangman <liangman@obook.com.cn>
2020-07-20 11:01:50 +0800

    修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockLookupActivity.java 	修改：     stock/app/src/main/res/layout/stock_lookup_layout.xml 	新文件：   stock/app/src/main/res/layout/stock_lookup_listviewitem.xml

--- stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockLookupActivity.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockLookupActivity.java
@@ -3,21 +3,42 @@ package com.example.stock.eink.pioneer.stocklookup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.stock.R;
+import com.example.stock.eink.pioneer.stockadd.StockAddActivity;
 
 public class StockLookupActivity extends AppCompatActivity {
+
+    private String stockTypeName;
+    private ImageView quitImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        TextView titleView = (TextView)findViewById(R.id.textview_title_stocktype);
-//        stockTypeName = getIntent().getStringExtra("stockTypeName");
-//        titleView.setText(stockTypeName);
-
-
         setContentView(R.layout.stock_lookup_layout);
+
+        TextView titleView = (TextView)findViewById(R.id.textview_title_stocktype);
+        stockTypeName = getIntent().getStringExtra("name");
+        titleView.setText(stockTypeName);
+
+        quitImageView = (ImageView)findViewById(R.id.imageView_title_quit);
+        quitImageView.setOnClickListener(listenerQuit);
+
+
+
     }
+
+
+    //@退出界面
+    private View.OnClickListener listenerQuit = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            StockLookupActivity.this.finish();
+        }
+    };
+
+
 }
