2d63e8936648405002e41967b353d6bfb9c973ef
liangman <liangman@obook.com.cn>
2020-07-21 10:33:38 +0800

    修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsActivity.java 	修改：     stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsDataHandle.java

--- stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsActivity.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,8 +13,14 @@ import com.example.stock.R;
 
 public class StockDetailsActivity extends AppCompatActivity {
 
+    private StockDetailsDataHandle dataHandler;
+
     private  ImageView quitImageView;
     private String stockName;
+    private TextView stockCodeTextView;
+
+    private EditText calpricEdittext;
+    private EditText gradeEdittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,21 @@ public class StockDetailsActivity extends AppCompatActivity {
         stockName = getIntent().getStringExtra("name");
         titleView.setText(stockName);
 
+        stockCodeTextView = (TextView)findViewById(R.id.textview_title_stockcode);
+
+        calpricEdittext = (EditText)findViewById(R.id.stock_calprice_editview);
+        gradeEdittext = (EditText)findViewById(R.id.stock_grade_edit);
+        Button saveCalpriceBut = (Button)findViewById(R.id.stock_savemcalpri_button);
+        saveCalpriceBut.setOnClickListener(saveCalPriceListner);
+
+
+        dataHandler = new StockDetailsDataHandle(this);
+        dataHandler.ObatinStockInfo(stockName);
+    }
+
+    public void InitData(StockData data){
+        stockCodeTextView.setText(data.code);
+        calpricEdittext.setText(data.calPrice);
     }
 
 
@@ -33,7 +56,17 @@ public class StockDetailsActivity extends AppCompatActivity {
     private View.OnClickListener listenerQuit = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            dataHandler.quit();
             StockDetailsActivity.this.finish();
+        }
+    };
+
+    //保存预测价格
+    private View.OnClickListener saveCalPriceListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dataHandler.saveCalprice(stockCodeTextView.getText().toString(),
+                    calpricEdittext.getText().toString(),gradeEdittext.getText().toString());
         }
     };
 
--- stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsDataHandle.java
+++ stock/app/src/main/java/com/example/stock/eink/pioneer/stocklookup/StockDetailsDataHandle.java
@@ -1,4 +1,79 @@
 package com.example.stock.eink.pioneer.stocklookup;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.stock.MyApplication;
+import com.example.stock.eink.lib.filesystem.ConfigParameter;
+import com.example.stock.eink.lib.filesystem.FileSystemManager;
+import com.example.stock.eink.lib.sqlite.MySqliteOpenHelper;
+
 public class StockDetailsDataHandle {
+
+    private MyApplication normalActivity;
+
+    private String databaseName = "stock.db";
+    private String databasePath;
+    private SQLiteDatabase db;
+
+    private StockDetailsActivity StockDetailsPt;
+
+    public StockDetailsDataHandle(StockDetailsActivity pt) {
+        StockDetailsPt = pt;
+
+        FileSystemManager fileManeger = new FileSystemManager();
+        databasePath = fileManeger.getStockFilePath() + databaseName;
+        normalActivity = new MyApplication();
+
+        int stockVersion = ConfigParameter.StockDatabaseVersion;
+        MySqliteOpenHelper oh = new MySqliteOpenHelper(normalActivity.getContext(), databasePath,
+                null, stockVersion);
+        db = oh.getWritableDatabase();
+    }
+
+
+    public void ObatinStockInfo(String stockName){
+        Cursor cursor = db.query("totalstock", new String[] {"name", "code","means","curprice","grap",
+                        "minprice","maxprice","calprice" },"name=?", new String[] { stockName },
+                null, null, null);
+
+        StockData data = new StockData();
+        // 将光标移动到下一行，从而判断该结果集是否还有下一条数据，如果有则返回true，没有则返回false
+        while (cursor.moveToNext()) {
+            //System.out.println("StockLookupDataHandle selectData result 444444444444444");
+            data.name = cursor.getString(cursor.getColumnIndex("name"));
+            data.code = cursor.getString(cursor.getColumnIndex("code"));
+            data.means = cursor.getString(cursor.getColumnIndex("means"));
+            data.curprice = cursor.getString(cursor.getColumnIndex("curprice"));
+            data.grap = cursor.getString(cursor.getColumnIndex("grap"));
+            data.yearMinPrice = cursor.getString(cursor.getColumnIndex("minprice"));
+            data.yearMaxPrice = cursor.getString(cursor.getColumnIndex("maxprice"));
+            data.calPrice = cursor.getString(cursor.getColumnIndex("calprice"));
+        }
+
+        StockDetailsPt.InitData(data);
+
+    }
+
+
+    public void saveCalprice(String stockCode,String price,String grade){
+        ContentValues values = new ContentValues();
+        values.put("calprice", price);
+        values.put("state", grade);
+        db.update("totalstock",values,"code=?",new String[] { stockCode });
+
+        if(stockCode.contains("sh60")){
+            db.update("shanghai",values,"code=?",new String[] { stockCode });
+        }else if(stockCode.contains("sz00")){
+            db.update("smallboard",values,"code=?",new String[] { stockCode });
+        }
+
+    }
+
+    public void quit(){
+        db.close();
+    }
+
+
 }
