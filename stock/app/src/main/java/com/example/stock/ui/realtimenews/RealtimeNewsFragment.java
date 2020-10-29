fcc181948894c90270a6eac6f06bb0a4f3e44a45
liangman <liangman@obook.com.cn>
2020-10-29 17:39:38 +0800

    修改：     stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java 	重命名：   stock/app/src/main/res/drawable/1.jpg -> stock/app/src/main/res/drawable/first.jpg 	修改：     stock/app/src/main/res/layout/realtime_news_layout.xml 	删除：     stock/app/src/main/res/mipmap-hdpi/1.jpg 	重命名：   stock/app/src/main/res/mipmap-anydpi-v26/1.jpg -> stock/app/src/main/res/mipmap-hdpi/first.jpg 	重命名：   stock/app/src/main/res/mipmap-hdpi/2.jpg -> stock/app/src/main/res/mipmap-hdpi/second.jpg 	重命名：   stock/app/src/main/res/mipmap-hdpi/3.jpg -> stock/app/src/main/res/mipmap-hdpi/thrid.jpg

--- stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java
+++ stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java
@@ -7,12 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.stock.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class RealtimeNewsFragment extends Fragment {
 
+    private ImageView realtimenewsImage;
+
+    // 定义开始等待时间  --- 等待 1 秒
+    private long delay = 2000;
+
+    // 定义每次执行的间隔时间
+    private long intevalPeriod = 2000;
+
+    private int index = 1;
+    private Timer timer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,8 +34,41 @@ public class RealtimeNewsFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.realtime_news_layout, container, false);
 
+        //realtimenewsImage = root.findViewById(R.id.realtimenews_imageview);
+
+        //timer = new Timer();
+        //timer.scheduleAtFixedRate(task, delay, intevalPeriod);
 
         return root;
 
     }
+
+    private TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            // 执行的输出的内容
+            if (index < 3){
+                index = index + 1;
+            }else{
+                index = 1;
+            }
+
+            switch (index){
+                case 1:
+                    System.out.println("144444444444444444444444444");
+                    //realtimenewsImage.setImageResource(R.mipmap.first);
+                    break;
+                case 2:
+                    System.out.println("244444444444444444444444444");
+                    //realtimenewsImage.setImageResource(R.mipmap.second);
+                    break;
+                case 3:
+                    System.out.println("344444444444444444444444444");
+                    //realtimenewsImage.setImageResource(R.mipmap.thrid);
+                    break;
+            }
+
+        }
+    };
+
 }
