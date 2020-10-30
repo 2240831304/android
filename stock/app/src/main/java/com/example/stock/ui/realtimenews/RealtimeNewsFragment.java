17909bfba0bc5c67533454ca852da487801ad7b2
liangman <liangman@obook.com.cn>
2020-10-30 16:36:37 +0800

    修改：     stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java 	修改：     stock/app/src/main/res/layout/realtime_news_layout.xml

--- stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java
+++ stock/app/src/main/java/com/example/stock/ui/realtimenews/RealtimeNewsFragment.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
@@ -12,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.TabWidget;
 
 import com.example.stock.R;
@@ -22,21 +22,9 @@ import java.util.TimerTask;
 
 public class RealtimeNewsFragment extends Fragment {
 
-    private ImageView realtimenewsImage;
-
-    // 定义开始等待时间  --- 等待 1 秒
-    private long delay = 2000;
-
-    // 定义每次执行的间隔时间
-    private long intevalPeriod = 2000;
-
-    private int index = 1;
-    private Timer timer;
-
     private String[] tabtitle = { "first", "second", "觉得基督","third","four","five","server","解决的角度" };
-    private TabWidget mTabWidget;
-    private ViewPager mViewPager;
 
+    private ViewPager mViewPager;
     private MyPagerAdapter mPagerAdapter;
 
     @Override
@@ -45,91 +33,37 @@ public class RealtimeNewsFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.realtime_news_layout, container, false);
 
-        //realtimenewsImage = root.findViewById(R.id.realtimenews_imageview);
-
-        //timer = new Timer();
-        //timer.scheduleAtFixedRate(task, delay, intevalPeriod);
-
-        mTabWidget = (TabWidget) root.findViewById(R.id.newstabWidget);
-        mTabWidget.setStripEnabled(false);
-        for (int i = 0;i < tabtitle.length;i++){
-            Button mBtnTabs = new Button(getActivity().getApplicationContext());
-            mBtnTabs.setFocusable(true);
-            mBtnTabs.setText(tabtitle[i]);
-            mBtnTabs.setOnClickListener(mTabClickListener);
-            mTabWidget.addView(mBtnTabs);
-        }
-
-        mViewPager = (ViewPager) root.findViewById(R.id.newsviewpager);
+        mViewPager = root.findViewById(R.id.newsviewpager);
         mPagerAdapter = new MyPagerAdapter(getFragmentManager(),tabtitle.length);
         mViewPager.setAdapter(mPagerAdapter);
-        mViewPager.setOnPageChangeListener(mPageChangeListener);
-
-        mTabWidget.setCurrentTab(0);
+        mViewPager.addOnPageChangeListener(mPageChangeListener);
 
         return root;
 
     }
 
 
-    private View.OnClickListener mTabClickListener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v)
-        {
-            Button mBtnTabs = (Button) v;
-            mBtnTabs.setBackgroundColor(Color.parseColor("#FFFACD"));
-            mViewPager.setCurrentItem(1);
-        }
-    };
-
     private ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
         public void onPageSelected(int arg0)
         {
-            mTabWidget.setCurrentTab(arg0);
+            System.out.println("onPageSelected 44444444444444444444444444444444444");
         }
 
         @Override
         public void onPageScrolled(int arg0, float arg1, int arg2)
         {
-
+            System.out.println("onPageScrolled 44444444444444444444444444444444444");
         }
 
         @Override
         public void onPageScrollStateChanged(int arg0)
         {
-
+            System.out.println("onPageScrollStateChanged 44444444444444444444444444444444444");
         }
+
     };
 
-    private TimerTask task = new TimerTask() {
-        @Override
-        public void run() {
-            // 执行的输出的内容
-            if (index < 3){
-                index = index + 1;
-            }else{
-                index = 1;
-            }
-
-            switch (index){
-                case 1:
-                    System.out.println("144444444444444444444444444");
-                    //realtimenewsImage.setImageResource(R.mipmap.first);
-                    break;
-                case 2:
-                    System.out.println("244444444444444444444444444");
-                    //realtimenewsImage.setImageResource(R.mipmap.second);
-                    break;
-                case 3:
-                    System.out.println("344444444444444444444444444");
-                    //realtimenewsImage.setImageResource(R.mipmap.thrid);
-                    break;
-            }
-
-        }
-    };
 
 }
