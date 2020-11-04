07414caea216c9886d9c39f72686790d90cd22d4
liangman <liangman@obook.com.cn>
2020-11-04 17:59:34 +0800

    修改：     app/src/main/java/com/example/stock/ui/realtimenews/VedioFrame.java

--- stock/app/src/main/java/com/example/stock/ui/realtimenews/VedioFrame.java
+++ stock/app/src/main/java/com/example/stock/ui/realtimenews/VedioFrame.java
@@ -1,10 +1,12 @@
 package com.example.stock.ui.realtimenews;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,11 +18,12 @@ public class VedioFrame extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Button mBtnTabs = new Button(getActivity().getApplicationContext());
-        mBtnTabs.setFocusable(false);
-        mBtnTabs.setClickable(false);
-        mBtnTabs.setText("视频专区");
-        View v = (View)mBtnTabs;
+        TextView textView = new TextView(getActivity().getApplicationContext());
+        textView.setFocusable(false);
+        textView.setClickable(false);
+        textView.setTextColor(Color.RED);
+        textView.setText("视频专区");
+        View v = (View)textView;
         return v;
     }
 
